d4baed3d8c4f0d05e58236196b4a10cf181263ea
christian7974 <ctropeano@uri.edu>
2025-02-03 18:56:24 -0500

    Abstracted fetchDogs, newSearch Functions
    Moved most of the logic from fetching dogs and handling a new search to a different file.

--- app/dogsScreen/helperFunctions.ts
+++ app/dogsScreen/helperFunctions.ts
@@ -1,5 +1,9 @@
 export const PAGE_SIZE = 12;
 
+/**
+ * 
+ * @returns list of dog breeds
+ */
 export async function fetchBreeds() {
     const response = await fetch(`${process.env.BASE_URL}/dogs/breeds`, {
         method: "GET",
@@ -18,7 +22,7 @@ export async function fetchBreeds() {
 }
 
 /**
-         * Function that fetches the dog search results (stored as a QueryResult object)
+* Function that fetches the dog search results (stored as a QueryResult object)
 */
 export async function fetchDogSearch(searchParams: searchParameters) {
     // add the parameters to the search results here
@@ -43,4 +47,70 @@ export async function fetchDogSearch(searchParams: searchParameters) {
     } catch (error) {
         console.error("Error fetching breeds:", error);
     }
+}
+
+/**
+ * Function to retrieve the list of dogs from the search
+ * @param dogIdArray array of dog ids from the search
+*/
+export async function fetchDogs(dogIdArray: string[]) {
+    try {
+        const response = await fetch(`${process.env.BASE_URL}/dogs`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            // body is the array of dog ids from the search
+            body: JSON.stringify(dogIdArray),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            const sortedDogs = data.sort((a: Dog, b: Dog) => a.breed.localeCompare(b.breed));
+            return(sortedDogs);
+        } else {
+            console.error("Failed to fetch dogs");
+        }
+    } catch (error) {
+        console.error("Error fetching dogs:", error);
+    }
+}
+
+export function processSearchForm(formData: FormData) {
+    const sortDataGroup = formData.get('sort_by') as string;
+        const sortOrder = formData.get('sort_order') as string;
+        var sortString = "";
+        if (sortDataGroup && sortOrder) {
+            sortString = `${sortDataGroup}:${sortOrder}`;
+        }
+
+        const ageMin = formData.get('ageMin') ? parseInt(formData.get('ageMin') as string) : undefined;
+        const ageMax = formData.get('ageMax') ? parseInt(formData.get('ageMax') as string) : undefined;
+
+        const zipCodesList = formData.get('zipCodes') as string;
+        const zipCodes = zipCodesList ? zipCodesList.split('\n') : [];
+
+        const breedsList = formData.getAll('breed') as string[];
+        const size = parseInt(formData.get('numReturn') as string);
+        const searchParams: searchParameters = {};
+        if (ageMin !== undefined) {
+            searchParams.ageMin = ageMin;
+        }
+        if (ageMax !== undefined) {
+            searchParams.ageMax = ageMax;
+        }
+        if (size !== undefined) {
+            searchParams.size = size;
+        }
+        if (breedsList.length > 0) {
+            searchParams.breeds = breedsList;
+        }
+        if (sortString !== "") {
+            searchParams.sort = sortString;
+        }
+        if (zipCodes.length > 0) {
+            searchParams.zipCodes = zipCodes;
+        }
+        return searchParams;    
 }
--- app/dogsScreen/page.tsx
+++ app/dogsScreen/page.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 import IndividualDog from "../components/IndividualDog";
-import { fetchBreeds, fetchDogSearch, PAGE_SIZE } from "./helperFunctions";
+import { fetchBreeds, fetchDogs, fetchDogSearch, PAGE_SIZE, processSearchForm } from "./helperFunctions";
 export default function dogsScreen() {
 
     // Const value to represent the maximum number of dogs to be shown
@@ -66,10 +66,6 @@ export default function dogsScreen() {
 
     // get the object from the dogs/search endpoint
     useEffect(() => {
-        /**
-         * Function that fetches the dog search results (stored as a QueryResult object)
-         */
-
         async function getDogSearchResults(searchQueries: searchParameters) {
             try {
                 const data = await fetchDogSearch(searchQueries);
@@ -84,36 +80,17 @@ export default function dogsScreen() {
 
     // using the dog ids, actually get the dogs
     useEffect(() => {
-        /**
-         * Function to retrieve the list of dogs from the search
-         * @param dogIdArray array of dog ids from the search
-         */
-        async function fetchDogs(dogIdArray: string[]) {
+        async function getDogs(dogIdArray: string[]) {
             try {
-                const response = await fetch(`${process.env.BASE_URL}/dogs`, {
-                    method: "POST",
-                    credentials: "include",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    // body is the array of dog ids from the search
-                    body: JSON.stringify(dogIdArray),
-                });
-
-                if (response.ok) {
-                    const data = await response.json();
-                    const sortedDogs = data.sort((a: Dog, b: Dog) => a.breed.localeCompare(b.breed));
-                    setDogs(sortedDogs);
-                } else {
-                    console.error("Failed to fetch dogs");
-                }
+                const data = await fetchDogs(dogIdArray);
+                setDogs(data);
             } catch (error) {
                 console.error("Error fetching dogs:", error);
             }
         }
         if (dogSearchResults && dogSearchResults.resultIds.length > 0) {
             // pass in ONLY the dog ids from the QueryResult object
-            fetchDogs(dogSearchResults.resultIds);
+            getDogs(dogSearchResults.resultIds);
         }
     }, [dogSearchResults?.resultIds]);
 
@@ -121,42 +98,10 @@ export default function dogsScreen() {
         event.preventDefault();
         const formData = new FormData(event.target as HTMLFormElement);
         updateFromPointer(0); // when we do a new search, reset the from pointer
-        const sortDataGroup = formData.get('sort_by') as string;
-        const sortOrder = formData.get('sort_order') as string;
-        var sortString = "";
-        if (sortDataGroup && sortOrder) {
-            sortString = `${sortDataGroup}:${sortOrder}`;
-        }
-
-        const ageMin = formData.get('ageMin') ? parseInt(formData.get('ageMin') as string) : undefined;
-        const ageMax = formData.get('ageMax') ? parseInt(formData.get('ageMax') as string) : undefined;
-
-        const zipCodesList = formData.get('zipCodes') as string;
-        const zipCodes = zipCodesList ? zipCodesList.split('\n') : [];
-
-        const breedsList = formData.getAll('breed') as string[];
-        const size = parseInt(formData.get('numReturn') as string);
-        const searchParams: searchParameters = {};
-        searchParams.from = fromPointer;
-        if (ageMin !== undefined) {
-            searchParams.ageMin = ageMin;
-        }
-        if (ageMax !== undefined) {
-            searchParams.ageMax = ageMax;
-        }
-        if (size !== undefined) {
-            searchParams.size = size;
-        }
-        if (breedsList.length > 0) {
-            searchParams.breeds = breedsList;
-        }
-        if (sortString !== "") {
-            searchParams.sort = sortString;
-        }
-        if (zipCodes.length > 0) {
-            searchParams.zipCodes = zipCodes;
-        }
+        const searchParams = processSearchForm(formData);
         updateSearchQueries(searchParams);
+
+
     }
 
     function handleNextPageClick() {
@@ -184,9 +129,6 @@ export default function dogsScreen() {
             updateFavoritedDogs(newFavoritedList);
             return;
         }
-        /**
-         * add dog to favorites array
-        */
         updateFavoritedDogs([...favoritedDogs, selectedDog]);
     }
 
@@ -301,9 +243,9 @@ export default function dogsScreen() {
                     })}
                 </div>
                 {favoritedDogs.length > 0 && <button onClick={() => handleFavoritedClick()}>Get Match</button>}
-                
-                    {matchedDog && <IndividualDog {...matchedDog} />}
-                
+
+                {matchedDog && <IndividualDog {...matchedDog} />}
+
             </main>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
