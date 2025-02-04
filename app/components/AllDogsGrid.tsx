f73b869fd3a63381562cdf643c654575cfeeb0ce
christian7974 <ctropeano@uri.edu>
2025-02-03 21:20:52 -0500

    Formatted IndividualDog, FavoritedDogGrid
    Formatted the IndividualDog, FavoritedDogGrid; make sure the breed name would not make the IndividualDog card too large.

--- app/components/AllDogsGrid.tsx
+++ app/components/AllDogsGrid.tsx
@@ -7,7 +7,7 @@ interface AllDogsGridProps {
 
 export default function AllDogsGrid({dogs, handleDogClick}: AllDogsGridProps) {
     return (
-        <div className="grid grid-cols-4 gap-4 bg-green-200">
+        <div className="grid grid-cols-2 xl:grid-cols-4 gap-4 bg-green-200 px-4 py-2 text-center">
             {dogs.map((dog) => {
                 return (
                     <div key={dog.id} onClick={() => handleDogClick(dog)}>
--- app/components/IndividualDog.tsx
+++ app/components/IndividualDog.tsx
@@ -1,13 +1,13 @@
 export default function IndividualDog(dog: Dog) {
-    const textSize = "xl";
+    const textSize = "md";
 
     return (
-        <div className="bg-red-500 w-100% rounded justify-items-center items-center">
+        <div className="bg-red-500 w-100% rounded justify-items-center items-center max-w-36 py-2 px-2 cursor-pointer">
             <p className={`text-${textSize}`}>{dog.name}</p>
             <p className={`text-${textSize}`}>{dog.age} years old</p>
-            <p className={`text-${textSize}`}>{dog.breed}</p>
+            <p className={`text-${textSize} w-3/4 truncate max-h-6`}>{dog.breed}</p>
             <p className={`text-${textSize}`}>{dog.zip_code}</p>
-            <img src={dog.img} className="h-16"/>
+            <img src={dog.img} className="h-20 rounded"/>
         </div>
     );
 }
